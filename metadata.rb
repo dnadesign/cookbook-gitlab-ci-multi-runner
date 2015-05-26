e1d3adbe0bd3bd26104f5f6242fad5cf0d6cc402
Jeremy Olliver <jeremy.olliver@gmail.com>
2015-05-26 16:48:23 +1200

    Initial cookbook generation

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'gitlab-ci-multi-runner'
+maintainer       'Jeremy Olliver'
+maintainer_email 'jeremy.olliver@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures gitlab-ci-multi-runner'
+long_description 'Installs/Configures gitlab-ci-multi-runner'
+version          '0.1.0'
